3f257f1dd980564f7ee93c112c3836237c969287
dash <dashc10@wgu.edu>
2024-01-28 00:47:28 -0800

    Fetch and display playerList[] from backend

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import NavBar from "./NavBar";
 
+interface Player {
+    steamid: string;
+    name: string;
+}
+
 const Player: React.FC = () => {
+
+    const [playerList, setPlayerList] = React.useState<Array<Player>>([]);
+
+    React.useEffect(() => {
+        fetch("/api/players")
+        .then((response) => response.json())
+        .then((data) => setPlayerList(data));
+    }, []);
+
   return (
     <div className="card">
     <NavBar />
     <ul>
-        <p>TODO:component player list</p>
+        {playerList.map((player) => (
+            <li key={player.steamid}> {player.name} </li>
+        ))}
     </ul>
     </div>
   );
