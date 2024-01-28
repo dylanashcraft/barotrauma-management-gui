f6aa572392393cb8202fc1c8d92cf414477ed5f9
dash <dashc10@wgu.edu>
2024-01-28 00:00:45 -0800

    Integrate routes and create Player page

---
+++ frontend/src/components/Player.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const Player: React.FC = () => {
+  return (
+    <div className="card">
+    <NavBar />
+    <ul>
+        <p>TODO:component player list</p>
+    </ul>
+    </div>
+  );
+};
+
+export default Player;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+
+import App from './App.tsx'
+import Player from './components/Player.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/players",
+    element: <Player />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
