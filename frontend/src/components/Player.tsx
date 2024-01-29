f10d365f2de2b7acf69da4d17cdfc123827735af
dash <dashc10@wgu.edu>
2024-01-28 21:29:53 -0800

    fix: URL and add debug for console output

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -11,9 +11,12 @@ const Player: React.FC = () => {
     const [playerList, setPlayerList] = React.useState<Array<Player>>([]);
 
     React.useEffect(() => {
-        fetch("/api/players")
+        fetch("http://localhost:3000/api/players")
         .then((response) => response.json())
-        .then((data) => setPlayerList(data));
+        .then((data) => { 
+            setPlayerList(data);
+            console.log(data);
+        });
     }, []);
 
   return (
