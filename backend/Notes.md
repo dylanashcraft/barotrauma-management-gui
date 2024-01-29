3fbac72cde6262e034caf04208d9af900aabf854
dash <dashc10@wgu.edu>
2024-01-29 00:03:45 -0800

    Add backend post request to push data to console

---
+++ backend/Notes.md
@@ -0,0 +1,4 @@
+## Player Management
+### Recent Players
+* Array of objects to keep players until x time period.
+    * Enables banning of players who leave before ban is issued.
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,15 +7,26 @@ export interface Player {
     steamid: string;
     name: string;
 }
-const testData: Player[] = [{ steamid: '4484', name: 'Frank' }, { steamid: '8372', name: 'Tom_Ran' }];
+
+let frontendRequest: {name: string, command: string}[] = [];
+
+let playerList: Player[] = []; // Players that have joined since start.
+playerList.push({ steamid: '4484', name: 'Frank' }, { steamid: '8372', name: 'Tom_Ran' });
+
 app.use((req: any, res: any, next: any)=>{
     res.header("Access-Control-Allow-Origin", "*");
     return next();
 });
 
 app.get('/api/players', (req: any, res: any) => {
-    res.send(testData);
+    res.send(playerList);
     console.log(`Request '${req.body}' on '${req.url}'`);
+});
+
+app.post('/api/players', (req: any, res: any) => {
+    frontendRequest.push(req.body); // FIXME: may need to parse as JSON / ensure data is returned for name and command. Consider returning array of names and commands. #TODO: Use class method to init console command.
+    res.status(200).send('OK');
+    console.log(`Got '${req.body}' on '${req.url}'`);
 });
 
 app.listen(port, () => {
